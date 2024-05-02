4f89f9afaf7cb84b1bbe1ea51a73cb573f3d2f5b
Gustaf Ekström <gusek93@gmail.com>
2024-05-02 21:33:59 +0200

    layout foundation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import {
+  AppContainer,
+  BackGroundImg,
+  Navbar,
+  MainContent,
+  Footer,
+} from "./components/ui/main";
+import headerImage from "./assets/header.png";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <AppContainer>
+      <BackGroundImg src={headerImage} />
+      <Navbar>Navbar</Navbar>
+      <MainContent>Main Content</MainContent>
+      <Footer>Footer</Footer>
+    </AppContainer>
+  );
 };
 
 export default App;
---
+++ frontend/src/components/ui/main.ts
@@ -0,0 +1,32 @@
+
+import styled from'styled-components';
+
+
+export const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const BackGroundImg = styled.img`
+  width: 100%;
+  height: 300px;
+  
+`;
+
+export const Navbar = styled.div`
+  width: 100%;
+  height: 50px;
+  background-color: green;
+`;
+
+export const MainContent = styled.div`
+  width: 100%;
+  height: 800px;
+  background-color: coral;
+`;
+
+export const Footer = styled.div`
+  width: 100%;
+  height: 50px;
+  background-color: blue;
+`;
